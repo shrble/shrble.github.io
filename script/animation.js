2752a301f7a255592c4e1279c68ee46082d28c0f
shrble <codeshbl@gmail.com>
2017-10-04 19:13:37 +0300

    xx
    xx

--- script/animation.js
+++
@@ -1,9 +0,0 @@
-var headerInput = $('.header__input');
-headerInput.on('focus',()=>{
-    headerInput.css({'border-bottom':'2px solid rgb(66, 66, 66)'})
-    headerInput.attr('placeholder','');
-})
-headerInput.on('blur',()=>{
-    headerInput.css({'border-bottom':'2px solid rgb(203, 203, 203)'});
-    headerInput.attr('placeholder','write TODO');
-})
